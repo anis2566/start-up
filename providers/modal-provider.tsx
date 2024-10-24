368a954d2dfcdb488a84a038d864221b29740fd2
anis2566 <anichuranis1000@gmail.com>
2024-10-24 17:30:35 +0600

    faa

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { DeleteAuthorModal } from "@/app/dashboard/authors/_components/delete-modal";
 import { DeleteCategoryModal } from "@/app/dashboard/categories/_components/delete-modal";
 import { DeleteSubCategoryModal } from "@/app/dashboard/categories/[id]/sub/_components/delete-modal";
